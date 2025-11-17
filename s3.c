f3a6309683bc3982d7d1045ef4a1af57b63a699b
Cyllocoding <129837159+Cyllocoding@users.noreply.github.com>
2025-11-17 16:47:10 +0000

    Add files via upload

---
+++ s3.c
@@ -0,0 +1,86 @@
+#include "s3.h"
+
+///Simple for now, but will be expanded in a following section
+void construct_shell_prompt(char shell_prompt[])
+{
+    strcpy(shell_prompt, "[s3]$ ");
+}
+
+///Prints a shell prompt and reads input from the user
+void read_command_line(char line[])
+{
+    char shell_prompt[MAX_PROMPT_LEN];
+    construct_shell_prompt(shell_prompt);
+    printf("%s", shell_prompt);
+
+    ///See man page of fgets(...)
+    if (fgets(line, MAX_LINE, stdin) == NULL)
+    {
+        perror("fgets failed");
+        exit(1);
+    }
+    ///Remove newline (enter)
+    line[strlen(line) - 1] = '\0';
+}
+
+void parse_command(char line[], char *args[], int *argsc)
+{
+    ///Implements simple tokenization (space delimited)
+    ///Note: strtok puts '\0' (null) characters within the existing storage, 
+    ///to split it into logical cstrings.
+    ///There is no dynamic allocation.
+
+    ///See the man page of strtok(...)
+    char *token = strtok(line, " ");
+    *argsc = 0;
+    while (token != NULL && *argsc < MAX_ARGS - 1)
+    {
+        args[(*argsc)++] = token;
+        token = strtok(NULL, " ");
+    }
+    
+    args[*argsc] = NULL; ///args must be null terminated
+}
+
+///Launch related functions
+void child(char *args[], int argsc)
+{
+    ///Implement this function:
+
+    ///Use execvp to load the binary 
+    ///of the command specified in args[ARG_PROGNAME].
+    ///For reference, see the code in lecture 3.
+
+    execvp(args[ARG_PROGNAME], args);
+
+    
+}
+
+void launch_program(char *args[], int argsc)
+{
+    ///Implement this function:
+
+    ///fork() a child process.
+    ///In the child part of the code,
+    ///call child(args, argv)
+    ///For reference, see the code in lecture 2.
+
+    ///Handle the 'exit' command;
+    ///so that the shell, not the child process,
+    ///exits.
+
+
+    if(strcmp(args[ARG_PROGNAME] ,"exit") == 0 ){
+        exit(1);
+    }
+    
+    int rc = fork();
+    if (rc < 0) { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) { // child (new process)
+        child(args, argsc);
+    } else { // parent goes down this path 
+        int wc = wait(NULL);
+    }
+}
